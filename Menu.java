5272674e58cc62302231339bedab7312bd2b4155
MuhammadAkmalNabil <113677891+MuhammadAkmalNabil@users.noreply.github.com>
2023-06-23 14:54:40 +0700

    Add files via upload

---
+++ Menu.java
@@ -0,0 +1,47 @@
+
+package baru;
+class menu{
+    public String Biasa;
+    public String Double;
+    public String Special;
+    
+    public void MenuPesanan(){
+//        this.Biasa =0;
+        System.out.println("======Menu======");
+//        swiche
+        System.out.println("1. Pesan Menu");
+//        case;
+        System.out.println("2. Lihat Pesanan");
+//        case;
+        System.out.println("3. Selesaikan Pesanan");
+    }
+    
+    public void Menu1(){
+        System.out.println("======Paket Menu Yang Tersedia=====");
+        
+        System.out.println("1. Biasa (Rp. 5000.0)");
+        
+        System.out.println("2. Double (Rp. 7000.0)");
+        
+        System.out.println("3. Spesial (Rp. 9000.0)");
+        
+    }
+}
+
+
+public class Menu {
+    public static void main(String[] args) {
+        Biasa ayam = new Biasa();
+        ayam.Biasa()=Biasa(ayam);
+        ayam.Biasa();
+        
+        
+        
+
+        System.out.println("=====Selamat datang di Ayam Geprek Kamal=====");
+        System.out.println("");
+    }
+ 
+
+    
+}
